cb9205d400f5c7a7cadfb75c49e3efb11b8be07b
KateSt421 <belporibet@mail.ru>
2025-05-16 22:32:45 +0300

    Added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
-import './index.css'
+import React, { useState } from "react";
+import './index.css';
 import Tariff from './components/Tariff';
 import tariffs from './data';
+import Header from './components/Header';
 
 const TariffList = () => {
+    const [selectedTariff, setSelectedTariff] = useState("");
+
+    const handleTariffClick = (name) => {
+        setSelectedTariff(name);
+    };
+
     return (
-        <div className="tariff-list">
-            {tariffs.map((tariff, index) => (
-                <Tariff 
-                    key={index} 
-                    name={tariff.name} 
-                    price={tariff.price} 
-                    speed={tariff.speed} 
-                    volume={tariff.volume}
-                    isHighlighted={tariff.price === 550} 
-                />
-            ))}
-        </div>
+        <>
+            <Header />
+            <div className="tariff-list"> 
+                {tariffs.map((tariff, index) => (
+                    <Tariff 
+                        key={index} 
+                        name={tariff.name} 
+                        price={tariff.price} 
+                        speed={tariff.speed} 
+                        volume={tariff.volume}
+                        isHighlighted={tariff.price === 550} 
+                        onClick={() => handleTariffClick(tariff.name)} 
+                    />
+                ))}
+                {selectedTariff && <h2>Вы выбрали тариф: {selectedTariff}</h2>}
+            </div>
+        </>
     );
 };
 
 export default TariffList;
+
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 function Header() {
-  return
-//   <header>
-// <h1>Список тарифов</h1>
-// </header>
+  return (
+  <header>
+<h1 className="title">Список тарифов</h1>
+</header> 
+)
 }
 
 export default Header;
